5205e97168d8c12ffd4b4824403e2a4b28a271f5
knoxaramav2 <wtscott@iastate.edu>
2021-02-05 15:46:34 -0600

    default instruction stubs

--- defaultInstructions.cpp
+++ defaultInstructions.cpp
@@ -7,10 +7,72 @@ using namespace std;
 using namespace KASM;
 
 int _noOp (Instruction&inst, InstructionFrame*frame){
-
-    cout << "NOOP called" << endl;
-
     return 0;
+}
+
+//general
+int _mov (Instruction&inst, InstructionFrame*frame){
+    return -1;
+}
+
+int _exit (Instruction&inst, InstructionFrame*frame){
+    return -1;
+}
+
+//math
+int _add (Instruction&inst, InstructionFrame*frame){
+    return -1;
+}
+
+int _mult (Instruction&inst, InstructionFrame*frame){
+    return -1;
+}
+
+int _div (Instruction&inst, InstructionFrame*frame){
+    return -1;
+}
+
+int _pow (Instruction&inst, InstructionFrame*frame){
+    return -1;
+}
+
+//logic
+int _cmp (Instruction&inst, InstructionFrame*frame){
+    return -1;
+}
+
+int _jlss (Instruction&inst, InstructionFrame*frame){
+    return -1;
+}
+
+int _jgtr (Instruction&inst, InstructionFrame*frame){
+    return -1;
+}
+
+int _jeql (Instruction&inst, InstructionFrame*frame){
+    return -1;
+}
+
+int _jneql (Instruction&inst, InstructionFrame*frame){
+    return -1;
+}
+
+//control
+int _jmp (Instruction&inst, InstructionFrame*frame){
+    return -1;
+}
+
+int _call (Instruction&inst, InstructionFrame*frame){
+    return -1;
+}
+
+//stack
+int _push (Instruction&inst, InstructionFrame*frame){
+    return -1;
+}
+
+int _pop (Instruction&inst, InstructionFrame*frame){
+    return -1;
 }
 
 
